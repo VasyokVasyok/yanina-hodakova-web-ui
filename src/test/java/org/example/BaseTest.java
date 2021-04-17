c7c9671b6d3fdfba5c8d31e8ac3fc7c66afaed22
Vasyok-Vasyok <vasyokvasyok333@yandex.ru>
2021-04-17 13:12:09 +0300

    First commit

---
+++ src/test/java/org/example/BaseTest.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.example.common.Configuration.BASE_URL;
+import static org.example.common.Configuration.LOGIN_PATH;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get(BASE_URL + LOGIN_PATH);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        LogEntries browserLogs = driver.manage().logs().get(LogType.BROWSER);
+        List<LogEntry> allLogRows = browserLogs.getAll();
+        driver.quit();
+    }
+}
